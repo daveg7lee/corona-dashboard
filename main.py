f8df16df96da8709c60b220feff41a7e29cd9bb7
Dave <daveg7lee@gmail.com>
2021-05-03 10:17:49 +0900

    Country Time Series part One

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ totals_df = totals_df.rename(columns={"index": "condition"})
 countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
 countries_df = countries_df.groupby("Country_Region").sum().reset_index()
 
-conditions = ["confirmed", "deaths", "recovered"]
-
 
 final_df = None
 
