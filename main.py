ca841575f0d7162a2688daa1f6b88a2a2589c08a
Dave <daveg7lee@gmail.com>
2021-05-06 18:24:23 +0900

    Bars Figure

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ stylesheets = [
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
 
+bubble_map = px.scatter_geo(
+    countries_df,
+    size="Confirmed",
+    color="Confirmed",
+    hover_name="Country_Region",
+    locations="Country_Region",
+    locationmode="country names",
+    projection="natural earth",
+    hover_data={
+        "Confirmed": ":,2f",
+        "Deaths": ":,2f",
+        "Recovered": ":,2f",
+        "Country_Region": False,
+    },
+    size_max=50,
+    template="plotly_dark",
+)
+
 app.layout = html.Div(
     style={
         "minHeight": "100vh",
@@ -24,12 +42,14 @@ app.layout = html.Div(
             style={"textAlign": "center", "paddingTop": "50px", "marginBottom": 100},
             children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
         ),
-        html.Div(children=[html.Div(children=[make_table(countries_df)])]),
+        html.Div(
+            children=[
+                html.Div(children=[dcc.Graph(id="bubble_map", figure=bubble_map)]),
+                html.Div(children=[make_table(countries_df)]),
+            ]
+        ),
     ],
 )
-
-map_figure = px.scatter_geo(countries_df)
-map_figure.show()
 
 if __name__ == "__main__":
     app.run_server(debug=True)
