ddb9475d5d958d2e6290d6c54cad785ad39b59a0
Dave <daveg7lee@gmail.com>
2021-06-06 19:42:39 +0900

    [FIX] app.title Bug fixed

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ stylesheets = [
     "https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap",
 ]
 
-app.title = "Corona Dashboard"
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
+
+app.title = "Corona Dashboard"
 
 server = app.server
 
